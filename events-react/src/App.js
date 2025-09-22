3f7885918344f5f10b38099f8fde96ade39d1748
Artem Minin <artemminin.03@mail.ru>
2025-09-22 20:59:08 +0300

    useState

--- events-react/src/App.js
+++ events-react/src/App.js
@@ -169,30 +169,324 @@ import "./App.css";
 // 11. Квадрат <div>, который при наведении (onMouseEnter) пишет в консоль "Навёл", а при
 // уходе (onMouseLeave) — "Ушёл".
 
-function App() {
-  return (
-    <>
-      <div
-        onMouseEnter={() => {
-          console.log("Навёл");
-        }}
-        onMouseLeave={() => {
-          console.log("Ушёл");
-        }}
-        style={{
-          width: "200px",
-          height: "200px",
-          backgroundColor: "lightblue",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          fontSize: "20px",
-          cursor: "pointer",
-        }}
-      >
-        Наведи на меня
-      </div>
-    </>
-  );
-}
-export default App;
+// function App() {
+//   return (
+//     <>
+//       <div
+//         onMouseEnter={() => {
+//           console.log("Навёл");
+//         }}
+//         onMouseLeave={() => {
+//           console.log("Ушёл");
+//         }}
+//         style={{
+//           width: "200px",
+//           height: "200px",
+//           backgroundColor: "lightblue",
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           fontSize: "20px",
+//           cursor: "pointer",
+//         }}
+//       >
+//         Наведи на меня
+//       </div>
+//     </>
+//   );
+// }
+// export default App;
+
+//Use State
+
+//  1. Создайте компонент с полем ввода и параграфом. При вводе данных в input отображать
+// вводимое значение в параграф текущей страницы
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState("");
+
+//   return (
+//     <>
+//       <input
+//         onChange={(e) => {
+//           b(e.target.value);
+//         }}
+//       />
+//       <p>{a}</p>
+//     </>
+//   );
+// }
+// export default App;
+
+// 2. Создайте компонент с полем ввода и кнопкой. По клику на кнопку собрать данные из input
+// и проверить вводимую строку на палиндром. Результат отображать в консоль
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState("");
+
+//   return (
+//     <>
+//       <input
+//         onChange={(e) => {
+//           b(e.target.value);
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           a.split("").reverse().join("") === a ? console.log(true) : console.log(false);
+//         }}
+//       >
+//         click
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 3. Создайте компонент с полем ввода и кнопкой. По клику на кнопку собрать данные из input
+// и проверить вводимую строку на почту. Результат отображать в консоль
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState("");
+
+//   return (
+//     <>
+//       <input
+//         onChange={(e) => {
+//           b(e.target.value);
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(a) ? console.log(true) : console.log(false);
+//         }}
+//       >
+//         click
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 4. Создайте компонент с 2 полями ввода и кнопкой. По клику на кнопку собрать данные из
+// двух input (2 разных state). Сравнить значения 2 input на равенство. Результат отображать в
+// консоль
+
+// import { useState } from "react";
+
+// function App() {
+//   const [inp1, setInp1] = useState("");
+//   const [inp2, setInp2] = useState("");
+
+//   const handleClick = () => {
+//     if (inp1 === inp2) {
+//       console.log(true);
+//     } else {
+//       console.log(false);
+//     }
+//   };
+//   return (
+//     <>
+//       <input onChange={(e) => setInp1(e.target.value)} />
+//       <input onChange={(e) => setInp2(e.target.value)} />
+//       <button onClick={handleClick}>click</button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 5. Создайте компонент с полем ввода имени и кнопкой "Привет". При вводе имени в поле и
+// нажатии на кнопку, компонент должен отображать приветственное сообщение с именем,
+// введенным пользователем
+
+// import { useState } from "react";
+
+// function App() {
+//   const [name, setName] = useState("");
+//   const [a, b] = useState("");
+//   const func = () => {
+//     b(name);
+//   };
+
+//   return (
+//     <>
+//       <input
+//         onChange={(e) => {
+//           setName(e.target.value);
+//         }}
+//       />
+//       <button onClick={func}>Привет</button>
+//       <h2>Привет {a}</h2>
+//     </>
+//   );
+// }
+// export default App;
+
+// 6. Создайте компонент с кнопкой "Показать/Скрыть текст". При нажатии на кнопку текст
+// должен появляться или исчезать
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(false);
+
+//   return (
+//     <>
+//       <p style={{ display: a ? "block" : "none" }}>name</p>
+//       <button onClick={() => b(!a)}>Привет</button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 7. Создайте компонент с кнопкой и параграфом для отображения результата счетчика
+// (изначально счетчик равен 0). Ваша задача так реализовать функционал, чтобы при клике на
+// кнопку автоматически прибавлялась +1 к стейту счетчика
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(0);
+
+//   return (
+//     <>
+//       <p>{a}</p>
+//       <button
+//         onClick={() => {
+//           b(a + 1);
+//         }}
+//       >
+//         +1
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 8. Дополните предыдущую задачу. Создайте компонент с 2 кнопками и параграфом для
+// отображения результата счетчика (изначально счетчик равен 0). Ваша задача так
+// реализовать функционал, чтобы при клике на кнопку +1 автоматически прибавлялась +1 к
+// стейту счетчика, а при клике на кнопку -1, происходило вычитание значения 1
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(0);
+
+//   return (
+//     <>
+//       <p>{a}</p>
+//       <button
+//         onClick={() => {
+//           b(a + 1);
+//         }}
+//       >
+//         +1
+//       </button>
+//       <button
+//         onClick={() => {
+//           b(a - 1);
+//         }}
+//       >
+//         -1
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 9. Дополните предыдущую задачу. Создайте компонент, который отображает счетчик и кнопку
+// "Сброс". При нажатии на кнопку, счетчик должен сбрасываться в ноль. Используйте useState,
+// чтобы управлять значением счетчика.
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(0);
+
+//   return (
+//     <>
+//       <p>{a}</p>
+//       <button
+//         onClick={() => {
+//           b(a + 1);
+//         }}
+//       >
+//         +1
+//       </button>
+//       <button
+//         onClick={() => {
+//           b(a - 1);
+//         }}
+//       >
+//         -1
+//       </button>
+//       <button
+//         onClick={() => {
+//           b(0);
+//         }}
+//       >
+//         Сброс
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 10. Создайте кнопку с названием "Добавить цвет". При клике на эту кнопку цвет шрифта
+// заголовка H1 должен измениться. При следующем клике цвет должен вернуться в
+// первоначальное состояние. Текст кнопки также должен меняться с "Открыто" на "Закрыто"
+// при нажатии на саму кнопку.
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(false);
+
+//   return (
+//     <>
+//       <h1 style={{ color: a ? "black" : "red" }}>{a ? "Открыто" : "Закрыто"}</h1>
+//       <button
+//         onClick={() => {
+//           b(!a);
+//         }}
+//       >
+//         Добавить цвет
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
+
+// 11. Напишите компонент, включающий в себя выезжающее меню из трех параграфов. По
+// нажатию на кнопку "Открыть" меню должно отображаться.
+
+// import { useState } from "react";
+
+// function App() {
+//   const [a, b] = useState(false);
+
+//   return (
+//     <>
+//       <div style={{ display: a ? "block" : "none" }}>
+//         <p>hi</p>
+//         <p>hi</p>
+//         <p>hi</p>
+//       </div>
+
+//       <button
+//         onClick={() => {
+//           b(!a);
+//         }}
+//       >
+//         Открыть
+//       </button>
+//     </>
+//   );
+// }
+// export default App;
